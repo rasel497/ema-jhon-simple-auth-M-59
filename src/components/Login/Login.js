d244d7776675cd3c183145c530974d8fe06c797c
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-17 17:53:15 +0600

    create Login componant and style Login from

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name='email' placeholder='your email' required />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' placeholder='your password' required />
+                </div>
+
+                <input className='btn-submit' type="submit" value="LOGIN" id="" />
+            </form>
+
+            <p>New to eama john? <Link to='/signup'>Create a new Account</Link></p>
         </div>
     );
 };
